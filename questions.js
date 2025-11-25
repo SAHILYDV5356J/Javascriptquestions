712b505631da0a474dd862c8c9780f97a9147e21
Sahil Yadav <sy05092005@gmail.com>
2025-11-25 15:24:31 +0530

    3changes

---
+++ questions.js
@@ -0,0 +1,12 @@
+// character is vowel or consonent
+
+var n= prompt("enter a character: ");
+
+if(n==='a' || n==='e' || n==='i' || n=== 'o' || n==='u' || n==='A' || n==='E' || n==='I' || n==='O' || n==='U' ){
+
+    console.log(`${n} is an vowel`);
+
+}
+else{
+    console.log(`${n} is an consonent`);
+}
