23307cf0c8772df171cbf10b5554248e8f9ad027
Sahil Yadav <sy05092005@gmail.com>
2025-12-03 02:13:21 +0530

    changetempV2

--- functions/degreetofaranite.js
+++ functions/degreetofaranite.js
@@ -5,30 +5,20 @@
 const faranite=(cel)=> (cel*1.8)+32 ;
 const celcius=(far)=>(far-32)/1.8;
 
-let input=(prompt("Enter 1 for Celcius to faranite\nEnter 2 for faranite to celcius\n "));
+let input=prompt("Enter 1 for Celcius to faranite\nEnter 2 for faranite to celcius\n ");
 
 while(true){
    
     if(Number(input)===1){
-    let value=prompt("Enter Your Temperature in Celcius:");
-
-    if(typeof(value) !==Number){
-        console.log("invalid input");
+        let value=prompt("Enter Your Temperature in Celcius:");
+        console.log(`The temperature in Faranite is : ${faranite(value)}`);
         break;
     }
-    console.log(`The temperature in Faranite is : ${faranite(value)}`);
-    break;
-   
-    }
+
     else if(Number(input)===2){
-     let value=prompt("Enter Your Temperature in Faranite:");
-
-     if(typeof(value) !==Number){
-        console.log("invalid input");
+        let value=prompt("Enter Your Temperature in Faranite:");
+        console.log(`The temperature in Celcius is : ${celcius(value)}`);
         break;
-    }
-     console.log(`The temperature in Celcius is : ${celcius(value)}`);
-     break;
     }
 
 
@@ -38,6 +28,9 @@ while(true){
     
     
 }
+
+
+
 
 
 
